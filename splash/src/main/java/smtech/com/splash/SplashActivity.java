dd46803ced036276cf4af38e1ed33965ee698899
Administrator <zhixindai@gmail.com>
2018-11-23 17:05:54 +0800

    添加app启动页模块

---
+++ splash/src/main/java/smtech/com/splash/SplashActivity.java
@@ -0,0 +1,14 @@
+package smtech.com.splash;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+//应用启动页
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
