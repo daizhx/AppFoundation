2ea1d0f3bd97f527dbd43b1ccbee31cb4d7cf9d7
Administrator <zhixindai@gmail.com>
2018-11-24 11:04:57 +0800

    fix

--- network/src/main/java/smtech/com/network/HttpClient.java
+++ network/src/main/java/smtech/com/network/HttpClient.java
@@ -1,5 +1,9 @@
 package smtech.com.network;
 
+import android.content.Context;
+import android.os.Handler;
+import android.os.Message;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -21,24 +25,46 @@ public class HttpClient {
     //local server ip
     private String serverHost;
     private int serverPort;
+    //执行http操作的上下文
+    private Context context;
+    private Handler handler;
 
     public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
 
     public HttpClient(String host,int port){
         this.serverHost = host;
         this.serverPort = port;
+        init();
     }
 
     //默认从HttpServerConfig获取服务器信息
     public HttpClient(){
-        serverHost = HttpServerConfig.getHost();
-        serverPort = HttpServerConfig.getPort();
+        init();
+    }
+
+    //
+    public HttpClient(Context context){
+        this.context = context;
+        handler = new Handler(context.getMainLooper());
+        init();
+    }
+
+    private void init(){
+        if(serverHost == null || serverHost.equals("")){
+            serverHost = HttpServerConfig.getHost();
+        }
+
+        if(serverPort == 0) {
+            serverPort = HttpServerConfig.getPort();
+        }
+
         if(serverHost == null || "".equals(serverHost)
                 || serverPort == 0){
             throw new RuntimeException("未配置服务器信息");
         }
     }
 
+    //获取服务器上的api的完整URL路径
     private String genUrl(String api){
         if(api == null){
             //不同请求用一个URL转发,通过不同参数来标识不同的请求
@@ -51,11 +77,12 @@ public class HttpClient {
         return "http://" + serverHost + ":" + serverPort +"/" + api;
     }
 
-    //异步提交数据接口
+
     public void post(String json, final HttpRequestCallback callback){
         post(null,json,callback);
     }
 
+    //异步提交数据接口
     public void post(String api, String json, final HttpRequestCallback callback){
         RequestBody body = RequestBody.create(JSON, json);
         Request request = new Request.Builder()
@@ -90,11 +117,64 @@ public class HttpClient {
         });
     }
 
-    //同步提交数据
+    //异步提交数据接口，请求完成的回调函数在主线程中执行
+    public void post2(String api, String json, final HttpRequestCallback callback){
+        if(context == null || handler == null){
+            throw new RuntimeException("调用该接口必须使用带Context参数的构造方法！");
+        }
+
+        RequestBody body = RequestBody.create(JSON, json);
+        Request request = new Request.Builder()
+                .url(genUrl(api))
+                .post(body)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, final IOException e) {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        callback.onFailure(e.getLocalizedMessage());
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                int httpCode = response.code();
+                if(httpCode != 200){
+                    callback.onFailure(response.message());
+                    return;
+                }
+
+                String ret = response.body().string();
+                JSONObject json = null;
+                try {
+                    json = new JSONObject(ret);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    json = new JSONObject();
+                }
+                final JSONObject finalJson = json;
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        callback.onSuccess(finalJson);
+                    }
+                });
+
+            }
+        });
+    }
+
+
+
     public String post(String json) throws IOException{
         return post(null,json);
     }
 
+    //同步提交数据
     public String post(String api,String json) throws IOException{
         RequestBody body = RequestBody.create(JSON,json);
         Request request = new Request.Builder().url(genUrl(api)).post(body).build();
@@ -107,12 +187,9 @@ public class HttpClient {
         return post2(null,json);
     }
 
+    //接口返回为json格式数据
     public JSONObject post2(String api,String json) throws IOException{
-        RequestBody body = RequestBody.create(JSON,json);
-        Request request = new Request.Builder().url(genUrl(api)).post(body).build();
-        Response res = client.newCall(request).execute();
-        String retStr= res.body().string();
-
+        String retStr= post(api,json);
         JSONObject ret = null;
         try {
             ret = new JSONObject(retStr);
@@ -127,19 +204,9 @@ public class HttpClient {
         return post3(null,json);
     }
 
+    //包装返回的json数据,对接口的通用字段进行处理，每个系统的业务接口应该会不一样，所以该方法不是通用方法，只是用来示例。
     public HttpRequestResult post3(String api,String json) throws IOException{
-        RequestBody body = RequestBody.create(JSON,json);
-        Request request = new Request.Builder().url(genUrl(api)).post(body).build();
-        Response res = client.newCall(request).execute();
-        String retStr= res.body().string();
-
-        JSONObject ret = null;
-        try {
-            ret = new JSONObject(retStr);
-        } catch (JSONException e) {
-            e.printStackTrace();
-            ret = new JSONObject();
-        }
+        JSONObject ret = post2(api,json);
         return new HttpRequestResult(ret);
     }
 }
